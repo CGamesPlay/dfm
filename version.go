987a7db9c2dfc90cf61380e48d9fca2ca56735f2
Ryan Patterson <cgamesplay@cgamesplay.com>
2019-12-08 23:49:58 +0400

    Bump to v1.1.0

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package main
 
 // Version is the version of the running dfm.
-const Version = "1.0.2"
+const Version = "1.1.0"
 
 // CopyrightString is used to show the license and author in help messages.
 const CopyrightString = `dfm, by Ryan Patterson, 2019
